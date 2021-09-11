65cab0f4943b23876627662b83df63b652986089
Mauro <emromero38@misena.edu.co>
2021-09-11 11:02:06 -0500

    Segundo commit Mauro

---
+++ target/generated-sources/annotations/com/barber/model/Factura_.java
@@ -0,0 +1,19 @@
+package com.barber.model;
+
+import com.barber.model.Calificacion;
+import com.barber.model.Cita;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-09-11T10:53:31")
+@StaticMetamodel(Factura.class)
+public class Factura_ { 
+
+    public static volatile SingularAttribute<Factura, Cita> citaIdCita;
+    public static volatile SingularAttribute<Factura, Float> costo;
+    public static volatile SingularAttribute<Factura, Integer> idFactura;
+    public static volatile ListAttribute<Factura, Calificacion> calificacionList;
+
+}
---
+++ target/generated-sources/annotations/com/barber/model/TipoRol_.java
@@ -0,0 +1,17 @@
+package com.barber.model;
+
+import com.barber.model.Usuario;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-09-11T10:53:31")
+@StaticMetamodel(TipoRol.class)
+public class TipoRol_ { 
+
+    public static volatile SingularAttribute<TipoRol, String> descripcion;
+    public static volatile ListAttribute<TipoRol, Usuario> usuarioList;
+    public static volatile SingularAttribute<TipoRol, Integer> numeroRol;
+
+}
